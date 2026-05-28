ff776b07493416cdb3915b3b4bdc55602d2ef502
David (secondary Dell) <davidswygart@gmail.com>
2026-05-28 19:14:49 -0400

    don't import current_status value

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import config
 import time
 import _thread
 import os
-from states import Status, current_status
+import states
+from states import Status
 from sd import mount_data_folder
 from hardware import clock, touch, button
 
@@ -12,22 +13,22 @@ def main():
 
 	print("Starting Main Loop")
 
-	current_status = Status.PENDING
+	states.current_status = Status.PENDING
 	while(True):
 		time.sleep(1)
 		# print("loop")
 		if button.value() == 0: # Is the button pressed?
 			print("Button Pressed")
-			if current_status == Status.PENDING:
+			if states.current_status == Status.PENDING:
 				print("Starting Recording Thread")
-				current_status = Status.RECORDING
+				states.current_status = Status.RECORDING
 				# run_experiment()
 				_thread.start_new_thread(run_experiment, ("Core1", 1))
 				# config.led.set_status("Recording")
 				time.sleep(1) # debounce
 			else:
 				print("Indicating that recording should stop")
-				current_status = Status.PENDING	
+				states.current_status = Status.PENDING	
 				time.sleep(5) # let it finish up
 				# led.set_status("Transferring")
 				# transfer_data()
@@ -44,7 +45,7 @@ def run_experiment(core, core_str):
 	filename = f'{now}_{name}.csv'
 	writer = DataWriter(filename)
 
-	while(current_status == Status.RECORDING):
+	while(states.current_status == Status.RECORDING):
 		c = touch.read()
 		t = clock.get_timestamp()
 		writer.write(t,c)
