003fd031767324c3be5713e974199557ea3f2679
David (secondary Dell) <davidswygart@gmail.com>
2026-05-28 19:22:21 -0400

    Add LED state display

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ SD_CS    = 14
 I2C_SCL = 9
 I2C_SDA = 8
 
+# RGB LED pins
+RGB_PWR_PIN = 2           # Power control for RGB LED
+RGB_PIN = 1               # Data pin for RGB LED
+
+# Input pins
 TOUCH_PIN = 1
 STOP_BUTTON_PIN = 38      # Press to stop recording and start Wi-Fi transfer
 
--- hardware.py
+++ hardware.py
@@ -1,8 +1,10 @@
 # Global variables
 from lib.rtc import init_rtc
 from machine import TouchPad, Pin
-from config import I2C_SCL, I2C_SDA, TOUCH_PIN, STOP_BUTTON_PIN
-
+from config import I2C_SCL, I2C_SDA, RGB_PIN, RGB_PWR_PIN, TOUCH_PIN, STOP_BUTTON_PIN
+import states
+from Led import LED
+led = LED(RGB_PWR_PIN, RGB_PIN, get_state=lambda: states.current_status)  # Pass getter function to read current state
 clock = init_rtc(scl_pin=I2C_SCL, sda_pin=I2C_SDA)
 touch = TouchPad(Pin(TOUCH_PIN))
 button = Pin(STOP_BUTTON_PIN, Pin.IN, Pin.PULL_UP)
