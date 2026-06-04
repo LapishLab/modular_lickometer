792b9e3eef29a9d4aaeaebffb2e4a708881faed6
David (secondary Dell) <davidswygart@gmail.com>
2026-06-04 10:59:55 -0400

    made changes to state to include the new LED error states

--- states.py
+++ states.py
@@ -4,10 +4,14 @@ class Status:
 	PENDING = [[0, 255, 0], 1]
 	RECORDING = [[0, 255, 0], 0]
 	STOPPING_RECORDING = [[255, 255, 0], 1]
-	DATA_TRANSFER = [[255, 255, 0], 2]
+	WIFI_CONNECTED = [[255, 255, 0], 2]
+	DATA_TRANSFER = [[255, 255, 0], 3]
 	
 	# Error states
 	ERROR_SD = [[255, 0, 0], 0]
 	ERROR_RTC = [[255, 0, 0], 3]
+	ERROR_LED = [[255, 0, 0], 1]
+	ERROR_TOUCH = [[255, 0, 0], 2]
+	ERROR_BUTTON = [[255, 0, 0], 4]
 	
 current_status = Status.STARTUP
