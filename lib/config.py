40906fc463cc8c24c2856670d5bf5ec7ba0c11af
David Swygart <davidswygart@gmail.com>
2026-09-24 14:32:25 -0400

    Update pins for PCB V1.0

--- lib/config.py
+++ lib/config.py
@@ -5,20 +5,23 @@ SD_CMD = 38
 SD_DETECT = 42  # Active low: 0 means a card is inserted
 
 # RTC pins
-I2C_SCL = 9
-I2C_SDA = 8
+I2C_SCL = 48
+I2C_SDA = 47
 
 # LED pins
-LED_REC_PIN = 15
-LED_TRANSFER_PIN = 14
-LED_ERROR_PIN = 16
+LED_REC_PIN = 14
+LED_TRANSFER_PIN = 13
+LED_ERROR_PIN = 12
 LOW_BATTERY_LED_PIN = 10
 
 # Battery voltage monitoring pin
 BATTERY_VOLTAGE_PIN = 8
+USING_BATTERY_PIN = 21
 
 # Input pins
-TOUCH_PINS = [1, 4]  # List of touch pins
+TOUCH_L_Pins = (7, 5, 11)  # Pins for left sipper (capacitance, reference, LED)
+TOUCH_R_Pins = (6, 4, 9) # Pins for right sipper (capacitance, reference, LED)
+
 START_BUTTON_PIN = 17     # Press to start recording
 STOP_BUTTON_PIN = 18      # Press to stop recording
 
@@ -35,6 +38,3 @@ from wifi_credentials import WIFI_SSID, WIFI_PASSWORD
 # DEVICE_HOSTNAME must be unique for every lickometer on the network.
 DEVICE_HOSTNAME = "lickometer-01"
 HTTP_SERVER_PORT = 80
-
-TCP_SERVER_HOST = "10.247.178.229"
-TCP_SERVER_PORT = 5000
